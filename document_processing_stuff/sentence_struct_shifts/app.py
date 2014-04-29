59e97188ad5701fb4343dcc890ecc9314cfd1e31
Justin Duda <justin.duda77@gmail.com>
2014-04-28 21:50:37 -0400

    some work

--- document_processing_stuff/sentence_struct_shifts/app.py
+++ document_processing_stuff/sentence_struct_shifts/app.py
@@ -1,1 +1,13 @@
-##
+import nltk
+
+
+def comp_sent(sent1, sent2):
+    sent1 = nltk.word_tokenize(sent1) 
+    sent2 = nltk.word_tokenize(sent2)
+    pts_spch1 = nltk.pos_tag(sent1)
+    pts_spch2 = nltk.pos_tag(sent2)
+
+    return 1
+
+a = "he crossed the street and jumped over a passing car"    
+b = "he ran across the street and scaled an oncoming vehicle"
