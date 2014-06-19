185498afd61881285a104e10355229b1fac81ea1
Justin Duda <justin.duda77@gmail.com>
2014-06-18 23:37:23 -0400

    work on app.py

--- app.py
+++ app.py
@@ -8,15 +8,19 @@ client = MongoClient()
 db = client['events']
 events = db['events']
 
-things = query_api(term, radius); 
-## something like this? ^^
 
 @app.route('/',methods=["POST","GET"])
 def Home():
+    
     if request.method == "GET":
         return render_template("index.html")
-    if request.method == "POST":
-        return redirect(url_for("/Home"));
+    if request.method == "POST":    
+        term = request.form['term']
+        radius = request.form['radius']
+        venues = query_api(term, radius)
+        #venues is a 2d list, i.e. [ [], [], [], ]
+        # each sublist should be like [ [location_as_string, name_of_venue, snippet_img_url, snippet_text], [same_for_second_venue,etc,etc,etc], [], ] for each venue
+        return render_template('index.html', venues = venues);
     
 
 
