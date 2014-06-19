d48e38a807c3f869cf16a49b6cb345db270b526c
Maia Ezratty <maiaviolet@gmail.com>
2014-06-18 23:24:32 -0400

    something like this to connect api and app?

--- app.py
+++ app.py
@@ -8,12 +8,17 @@ client = MongoClient()
 db = client['events']
 events = db['events']
 
+things = query_api(term, radius); 
+## something like this? ^^
+
 @app.route('/',methods=["POST","GET"])
 def Home():
     if request.method == "GET":
         return render_template("index.html")
     if request.method == "POST":
         return redirect(url_for("/Home"));
+    
+
 
 if __name__=="__main__":
     app.debug=True
